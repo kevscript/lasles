4d007a815fa9ee4ceec776ccce386d64068d7bef
Kevin Ostafinski <kevscripted@gmail.com>
2021-06-25 18:22:44 +0200

    init project + header

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+import "../styles/main.scss";
+
+import "../assets/amazon.svg";
+import "../assets/arrow.svg";
+import "../assets/discord.svg";
+import "../assets/facebook.svg";
+import "../assets/feature.svg";
+import "../assets/hero.svg";
+import "../assets/instagram.svg";
+import "../assets/logo.svg";
+import "../assets/map.svg";
+import "../assets/netflix.svg";
+import "../assets/plan.svg";
+import "../assets/reddit.svg";
+import "../assets/spotify.svg";
+import "../assets/star.svg";
+import "../assets/twitter.svg";
+import "../assets/avatar1.png";
+import "../assets/avatar2.png";
+import "../assets/avatar3.png";
